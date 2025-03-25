1cdae607514c292c4e7dd970add173101b13ffb2
lorenzosalti <lorenzosalti@gmail.com>
2025-03-25 16:28:28 +0100

    feat: add if else with for cycle to print every additional terminal argument

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 // string to log with null coalescing operator
-let argument = process.argv[2] ?? 'Hello Boolean';
+// let argument = process.argv[2] ?? 'Hello Boolean';
 
 // console.log(process.argv[2]);
 
 // console.log('Hello Boolean');
 
-console.log(argument);
+// console.log(argument);
+
+if (process.argv.length > 2) {
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
+} else {
+  console.log('Hello Boolean');
+}
