02495ae253554183eb490999b85fb40325b58670
lorenzosalti <lorenzosalti@gmail.com>
2025-03-25 16:35:50 +0100

    feat: add phrase variable to log arguments in line

--- index.js
+++ index.js
@@ -8,10 +8,30 @@
 
 // console.log(argument);
 
+// // instructions to log every argument
+// if (process.argv.length > 2) {
+//   for (let i = 2; i < process.argv.length; i++) {
+//     console.log(process.argv[i]);
+//   }
+// } else {
+//   console.log('Hello Boolean');
+// }
+
+// instructions to log every argument in line
 if (process.argv.length > 2) {
+
+  let phrase = '';
+
   for (let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i]);
+
+    phrase += `${process.argv[i]} `
+
   }
+
+  console.log(phrase);
+
 } else {
+
   console.log('Hello Boolean');
+
 }
