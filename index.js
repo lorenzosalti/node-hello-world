3b991858e3dd7333513b7a5117ec610cccf13765
lorenzosalti <lorenzosalti@gmail.com>
2025-03-25 16:20:47 +0100

    feat: add null coalescing operator to print argument only if specified

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log(process.argv[2]);
 
-console.log('Hello Boolean');
+// string to log with null coalescing operator
+let argument = process.argv[2] ?? 'Hello Boolean';
+
+// console.log(process.argv[2]);
+
+// console.log('Hello Boolean');
+
+console.log(argument);
